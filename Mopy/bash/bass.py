e41397d3fec40a1bc67ccf4969bc8d35e959d4ab
MrD <the.ubik@gmail.com>
2018-07-29 15:00:13 +0300

    307.201807291447

--- Mopy/bash/bass.py
+++ Mopy/bash/bass.py
@@ -29,7 +29,7 @@ and environment issues - do not modify or imitate (ut)."""
 # no imports
 
 language = None
-AppVersion = u"307" # must represent a valid float
+AppVersion = u"307.201807291447" # must represent a valid float
 
 #--Null strings (for default empty byte arrays)
 null1 = '\x00'
